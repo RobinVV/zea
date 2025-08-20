6650b9495f5ac792c9cc17dfdb4cd1451fa36fed
Wessel van Nierop <40076995+wesselvannierop@users.noreply.github.com>
2025-08-20 13:20:22 +0000

    pfield_kwargs is a dependency of pfield

--- zea/scan.py
+++ zea/scan.py
@@ -538,6 +538,7 @@ class Scan(Parameters):
         "tx_apodizations",
         "grid",
         "t0_delays",
+        "pfield_kwargs",
     )
     def pfield(self):
         """Compute or return the pressure field (pfield) for weighting."""
