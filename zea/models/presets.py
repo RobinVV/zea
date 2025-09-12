ddc5a24da5f3abbd6a3e544d6f887673756750ea
OisinNolan <oinolan@tcd.ie>
2025-09-12 14:01:18 +0000

    return of the ruff

--- zea/models/presets.py
+++ zea/models/presets.py
@@ -51,7 +51,8 @@ echonet_lvh_presets = {
     "echonetlvh": {
         "metadata": {
             "description": (
-                "EchoNetLVH segmentation model for PLAX-view cardiac ultrasound segmentation. Trained on images of size (224, 224)."
+                "EchoNetLVH segmentation model for PLAX-view cardiac ultrasound segmentation. "
+                "Trained on images of size (224, 224)."
             ),
             "params": 0,
             "path": "echonetlvh",
