ad2b645f444ac938544009968e144e9c326b6f6c
OisinNolan <oinolan@tcd.ie>
2025-09-22 13:34:58 +0000

    remove unnecessary params

--- zea/models/echonetlvh.py
+++ zea/models/echonetlvh.py
@@ -38,12 +38,6 @@ class EchoNetLVH(BaseModel):
         """
         super().__init__(**kwargs)
 
-      
-        # Network input/output dimensions
-        # self.n_rho = 224
-        # self.n_theta = 224
-        # self.output_shape = (224, 224, 4)
-
         # Pre-computed coordinate grid for efficient processing
         self.coordinate_grid = ops.stack(
             ops.cast(ops.convert_to_tensor(np.indices((224, 224))), "float32"), axis=-1
