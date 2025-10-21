Fix bug giving wrong shapes for default array with selected_transmits

zea/scan.py
@@ -360,6 +360,11 @@ class Scan(Parameters):
         """The total number of transmits in the full dataset."""
         return self._params["n_tx"]
 
+    @property
+    def n_tx_selected(self):
+        """The number of currently selected transmits."""
+        return len(self.selected_transmits)
+
     @cache_with_dependencies("selected_transmits")
     def n_tx(self):
         """The number of currently selected transmits."""
@@ -493,7 +498,7 @@ class Scan(Parameters):
         value = self._params.get("azimuth_angles")
         if value is None:
             log.warning("No azimuth angles provided, using zeros")
-            value = np.zeros(self.n_tx_total)
+            value = np.zeros(self.n_tx_selected)
 
         return value[self.selected_transmits]
 
@@ -504,7 +509,7 @@ class Scan(Parameters):
         value = self._params.get("t0_delays")
         if value is None:
             log.warning("No transmit delays provided, using zeros")
-            return np.zeros((self.n_tx_total, self.n_el))
+            return np.zeros((self.n_tx_selected, self.n_el))
 
         return value[self.selected_transmits]
 
@@ -514,7 +519,7 @@ class Scan(Parameters):
         value = self._params.get("tx_apodizations")
         if value is None:
             log.warning("No transmit apodizations provided, using ones")
-            value = np.ones((self.n_tx_total, self.n_el))
+            value = np.ones((self.n_tx_selected, self.n_el))
 
         return value[self.selected_transmits]
 
@@ -524,7 +529,7 @@ class Scan(Parameters):
         value = self._params.get("focus_distances")
         if value is None:
             log.warning("No focus distances provided, using zeros")
-            value = np.zeros(self.n_tx_total)
+            value = np.zeros(self.n_tx_selected)
 
         return value[self.selected_transmits]
 
@@ -534,7 +539,7 @@ class Scan(Parameters):
         value = self._params.get("initial_times")
         if value is None:
             log.warning("No initial times provided, using zeros")
-            value = np.zeros(self.n_tx_total)
+            value = np.zeros(self.n_tx_selected)
 
         return value[self.selected_transmits]
 
@@ -586,7 +591,7 @@ class Scan(Parameters):
         """Indices of the waveform used for each transmit event of shape (n_tx,)."""
         value = self._params.get("tx_waveform_indices")
         if value is None:
-            return np.zeros(self.n_tx_total, dtype=int)
+            return np.zeros(self.n_tx_selected, dtype=int)
 
         return value[self.selected_transmits]
 