a320987408489fffec8f5a0aba394f1854ac98dc
simon <s.w.penninga@tue.nl>
2025-11-19 13:36:07 +0000

    fix names of source when unzipping for all datasets

--- zea/data/convert/utils.py
+++ zea/data/convert/utils.py
@@ -23,24 +23,27 @@ def unzip(src: str | Path, dataset: str) -> Path:
     Checks if data folder exist in src.
     Otherwise, unzip dataset.zip in src.
     """
-
+    src = Path(src)
     if dataset == "picmus":
         zip_name = "picmus.zip"
         folder_name = "archive_to_download"
+        unzip_dir = src / folder_name
     elif dataset == "camus":
         zip_name = "CAMUS_public.zip"
         folder_name = "CAMUS_public"
+        unzip_dir = src / folder_name
     elif dataset == "echonet":
         zip_name = "EchoNet-Dynamic.zip"
         folder_name = "EchoNet-Dynamic"
+        unzip_dir = src / folder_name / "Videos"
     elif dataset == "echonetlvh":
         zip_name = "EchoNet-LVH.zip"
         folder_name = "Batch1"
+        unzip_dir = src
     else:
         log.error(f"Dataset {dataset} not recognized for unzip.")
         sys.exit(1)
 
-    src = Path(src)
     if (src / folder_name).exists():
         if dataset == "echonetlvh":
             # EchoNetLVH dataset unzips into four folders. Check they all exist.
@@ -51,10 +54,7 @@ def unzip(src: str | Path, dataset: str) -> Path:
                 f"Missing MeasurementsList.csv in {src}."
             )
             log.info(f"Found Batch1, Batch2, Batch3, Batch4 and MeasurementsList.csv in {src}.")
-            return src
-        else:
-            log.info(f"Found existing {folder_name} folder in {src}. Skipping unzip.")
-            return src / folder_name
+        return unzip_dir
 
     zip_path = src / zip_name
     if not zip_path.exists():
@@ -67,5 +67,7 @@ def unzip(src: str | Path, dataset: str) -> Path:
     with zipfile.ZipFile(zip_path, "r") as zip_ref:
         zip_ref.extractall(src)
     log.info("Unzipping completed.")
-    log.info(f"Starting conversion from {src / folder_name}.")
-    return src / folder_name if dataset != "echonetlvh" else src
+    log.info(f"Starting conversion from {src / folder_name}.")    
+    return unzip_dir
+    
+   
