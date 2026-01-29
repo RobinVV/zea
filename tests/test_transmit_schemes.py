601620378412e9539c8c18ebc625b939e57e8b55
simon <s.w.penninga@tue.nl>
2026-01-29 15:00:02 +0000

    remove batch-dim in 2 tests

--- tests/test_transmit_schemes.py
+++ tests/test_transmit_schemes.py
@@ -106,8 +106,8 @@ def _test_location(image, extent, true_position):
 @pytest.fixture
 def ultrasound_scatterers():
     """Returns scatterer positions and magnitudes for ultrasound simulation tests."""
-    scat_positions = fish()
-    n_scat = scat_positions.shape[0]
+    scat_positions = np.expand_dims(fish(), axis=0)
+    n_scat = scat_positions.shape[1]
 
     return {
         "positions": scat_positions.astype(np.float32),
@@ -241,8 +241,8 @@ def test_phantoms():
     lissajous_scat = lissajous(num_scatterers=50)
     golden_ratio_scat = golden_ratio(num_scatterers=50)
 
-    assert fish_scat.shape == (1, 104, 3)
-    assert rose_scat.shape == (1, 50, 3)
-    assert fibonacci_scat.shape == (1, 50, 3)
-    assert lissajous_scat.shape == (1, 50, 3)
-    assert golden_ratio_scat.shape == (1, 50, 3)
+    assert fish_scat.shape == (104, 3)
+    assert rose_scat.shape == (50, 3)
+    assert fibonacci_scat.shape == (50, 3)
+    assert lissajous_scat.shape == (50, 3)
+    assert golden_ratio_scat.shape == (50, 3)
